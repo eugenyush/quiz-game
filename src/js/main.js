e13a2847bdbb951d1240dd618a12644f42c11de1
Yauhen <yushkevicheugen@gmail.com>
2024-02-08 16:18:37 +0100

    Fixed bugs and applications at the final stage

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,30 @@ const answerText = document.querySelectorAll("#answer");
 const scoreQuestion = document.querySelector("#score");
 const btnSubmit = document.querySelector("#btn-submit");
 const btnStart = document.querySelector("#btn-start");
+const startPanel = document.querySelector('#app-start');
+const appQuestion = document.querySelector('#app-content');
+const appAnswers = document.querySelector('#app-answers');
+const themeSwitch = document.querySelector('#themeSwitch');
+const msgError = document.querySelector('.error-massg');
 
 let askedQuestions = [];
+let selectedAnswerIndex;
+let completedScore;
+let score;
+let currentQuestion;
+
+const rightAnswer = (score) => {
+
+    if(score > 1 && score < 5){
+        return `You got ${score} right answers. Maybe another time. Try again.`
+    }else if(score >= 5 && score < 8 ){
+        return `You got ${score} right answers. It's not that bad. Try again.`
+    }else if (score >= 8 && score <= 10){
+        return `You got ${score} right answers. It's great to keep it that way.`
+    }else{
+        return  `You got ${score} right answers. Don't worry, try again.`
+    }
+}
 
 const getRandomQuestion = () =>{
     const availableQuestions = data.filter((_, index) => !askedQuestions.includes(index));
@@ -28,22 +50,42 @@ const cheekTrueQuestion = (question,answer) =>{
     return question.options[answer].isCorrect;
 };
 
-let selectedAnswerIndex;
-let completedScore;
-let score;
-let currentQuestion;
+const handleIncorrectAnswer = () => {
+    console.log("false");
+    //Some active for fail answer
+};
+
+const updateScoreQuestion = () => {
+    scoreQuestion.textContent = `Question ${completedScore} of 10`;
+};
+
+
+const clearActiveAnswer = () => {
+    answerText.forEach(answer => answer.classList.remove('active'));
+};
 
 const handleQuizSubmission = () => {
     if (completedScore <= 10) {
         if (currentQuestion) {
-            cheekTrueQuestion(currentQuestion, selectedAnswerIndex) ? score++ : console.log("false");
-            completedScore++;
-            scoreQuestion.textContent = `Question ${completedScore} of 10`;
+            if (selectedAnswerIndex !== null && selectedAnswerIndex !== undefined) {
+                cheekTrueQuestion(currentQuestion, selectedAnswerIndex) ? score++ : handleIncorrectAnswer();
+                completedScore++;
+                updateScoreQuestion();
+            } else {
+                msgError.style.display = 'block';
+                
+                setTimeout(() => {
+                    msgError.style.display = 'none';
+                }, 3000);
+                return;
+            }
         }
         const question = getRandomQuestion();
         if (question) {
             displayQuestion(question);
             currentQuestion = question;
+            clearActiveAnswer();
+            selectedAnswerIndex = null;
         } else {
             endQuiz();
         }
@@ -52,21 +94,31 @@ const handleQuizSubmission = () => {
     }
 };
 
+
 const startQuiz = () => {
     btnStart.style.display = "none";
     btnSubmit.style.display = "block";
-    completedScore = 1;
-    scoreQuestion.textContent = `Question ${completedScore} of 10`;
+    appQuestion.style.display = "block";
+    appAnswers.style.display = "block";
+    startPanel.style.display = "none";
+
     score = 0;
-    //displayQuestion(getRandomQuestion());
-    handleQuizSubmission(); 
+    completedScore = 1;
+    updateScoreQuestion();
+    handleQuizSubmission();
 };
 
 const endQuiz = () => {
     btnStart.style.display = "block";
     btnSubmit.style.display = "none";
-    console.log(`Right answer ${score}/10`);
-    score = 0;
+    appQuestion.style.display = "none";
+    appAnswers.style.display = "none";
+    startPanel.style.display = "block";
+    startPanel.textContent =  rightAnswer(score);
+    completedScore = 0;
+    score = 0; 
+    currentQuestion = null; 
+    askedQuestions = []; 
 };
 
 const displayQuestion = (question) => {
@@ -78,7 +130,7 @@ const displayQuestion = (question) => {
 
 answerText.forEach((item,index) =>{
     item.addEventListener("click",() =>{
-        answerText.forEach(b => b.classList.remove('active'));
+        clearActiveAnswer();
         item.classList.add('active');
         selectedAnswerIndex = index;
     });
@@ -91,4 +143,13 @@ btnSubmit.addEventListener("click", () => {
 
 btnStart.addEventListener("click", () => {
     startQuiz();
+});
+
+themeSwitch.addEventListener("click", () => {
+    const currentTheme = document.body.className;
+    if (currentTheme === 'light-theme') {
+        document.body.className = 'dark-theme';
+    } else {
+        document.body.className = 'light-theme';
+    }
 });
